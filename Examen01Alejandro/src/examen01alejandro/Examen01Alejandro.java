fef43b3eb6dde666e80c9bd0edaeda84bb0964d9
AleSanchez0610 <salejandro80810@ufide.ac.cr>
2024-10-30 20:48:36 -0600

    Add files via upload

---
+++ Examen01Alejandro/src/examen01alejandro/Examen01Alejandro.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen01alejandro;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Examen01Alejandro {
+        // atributos factura 
+    public String nombreCliente;
+    private String cedula; 
+    private String codigoFactura;
+    private double montoFactura;
+    public int Mes;
+    private boolean contieneproductodosE;
+    private boolean contieneAutomotriz;
+    private boolean contieneConstrucion; 
+    
+     public static void main(String[] args) {
+     
+     public facturas(String nombreCliente, String cedula, String codigoFactura, double montoFactura, int Mes, int contieneproductosE, int contieneAutomotriz,  int contieneConstrucion,);
+             
+             
+  String mes = JOptionPane.showInputDialog("digite el numero del mes")
+     // digitar el numero del mes de la factura
+          
+  int meses= Integer.parseInt(mes);
+            switch (meses){
+    case 1:
+        System.out.println("enero");
+        break;
+    case 2:
+        System.out.println("febrero");
+        break;
+    case 3:
+        System.out.println("marzo");
+        break;
+    case 4:
+        System.out.println("abril");
+        break;
+     case 5:
+        System.out.println("mayo");
+         break;
+     case 6:
+         System.out.println("junio");
+         break;
+     case 7 : 
+        System.out.println("julio");
+         break;
+     case 8 : 
+         System.out.println("agosto");
+         break;
+     case 9:
+         System.out.println("septiembre");
+         break;
+     case 10:
+         System.out.println("octubre");
+         break;
+     case 11:
+         System.out.println("noviembre");
+       break;
+     case 12 
+       System.out.println("diciembre");
+}
+          }
+    
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+}
